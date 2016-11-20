736335120c029d72d0b51bec34728341254d604c
Allen <allenliu1991@icloud.com>
2016-11-20 18:16:31 +0800

    add a part to test Git

--- Python_CookBook/Data_Structure.py
+++ Python_CookBook/Data_Structure.py
@@ -91,5 +91,17 @@ print(heapq.heappop(heap))
 
 #实现一个优先级序列
 
+class PriorityQ:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self,item,priority):
+        heapq.heappush(self._queue, (-priority,self._index,item))
+        self._index += 1
+
+    def pop(self):
+        return  heapq.heappop(self._queue)[-1]
+
 
 
